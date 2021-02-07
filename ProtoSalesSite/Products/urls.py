9b67aeb22f534e4a35815f258e3ddd5de2541fed
MihaiTaban <mihai.taban.89@gmail.com>
2021-02-07 15:45:55 +0200

    update

--- ProtoSalesSite/Products/urls.py
+++ ProtoSalesSite/Products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "Products"
 urlpatterns = [
-    path('', views.homeview, name='homeview'),
+    path('', views.homepage, name='homepage'),
+
 ]
--- ProtoSalesSite/Products/views.py
+++ ProtoSalesSite/Products/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def homeview(request):
-    return HttpResponse("Hello, world. You're at the polls homeview.")
+def homepage(request):
+    return HttpResponse("Hello, world. You're at the <strong>Products</strong> homepage.")
