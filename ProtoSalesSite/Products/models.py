a2425d5d940ab9daee378d53b33459bd4d51822f
MihaiTaban <mihai.taban.89@gmail.com>
2021-02-07 16:52:18 +0200

    update

--- ProtoSalesSite/Products/models.py
+++ ProtoSalesSite/Products/models.py
@@ -2,4 +2,26 @@ from django.db import models
 
 # Create your models here.
 
-class BazaDeDate(models.Model):
+class ProductSectionsDB(models.Model):
+    Sections = models.CharField(max_length=200)
+
+class SkiDB(models.Model):
+    Brand = models.CharField(max_length=200)
+    Size = models.IntegerField(200)
+    Availability = models.IntegerField(200)
+
+class SnowboardDB(models.Model):
+    Brand = models.CharField(max_length=200)
+    Size = models.IntegerField(200)
+    Availability = models.IntegerField(200)
+
+class SkiBootsDB(models.Model):
+    Brand = models.CharField(max_length=200)
+    Size = models.IntegerField(200)
+    Availability = models.IntegerField(200)
+
+class SnowboardBootsDB(models.Model):
+    Brand = models.CharField(max_length=200)
+    Size = models.IntegerField(200)
+    Availability = models.IntegerField(200)
+
